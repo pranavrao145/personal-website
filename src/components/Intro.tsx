619be970ed09766cd22ca186da8f528e4c4f7e51
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2023-05-01 14:04:00 -0400

    feat(cta): added ctas on intro page

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -11,6 +11,24 @@ export default function Intro(props: IIdProps) {
           I'm a passionate and motivated computer science student at the
           University of Toronto, learning new things every day!
         </div>
+        <div className="text-lg md:text-3xl ml-1 mt-10 font-medium flex-wrap pr-10">
+          Scroll to take a look at some of my{" "}
+          <a className="underline" href="#projects">
+            {" "}
+            projects
+          </a>
+          , read more{" "}
+          <a className="underline" href="#about">
+            {" "}
+            about me
+          </a>
+          , or{" "}
+          <a className="underline" href="#contact">
+            {" "}
+            get in touch with me
+          </a>
+          !
+        </div>
       </div>
     </div>
   );
